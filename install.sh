089a8a5da98e0a836f10e5fa4b2296ef3cf1a6fc
Ben Hussey <ben@blip2.net>
2017-10-11 12:54:39 +0100

    Made more generic, includes gnome power saving changes and desktop background

--- install.sh
+++ install.sh
@@ -1,1 +1,15 @@
+gsettings set org.gnome.desktop.background picture-uri file:///home/iot/webkiosk/arup.png
+gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-timeout '0'
+gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-battery-timeout '0'
+gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-battery-type 'nothing'
+gsettings set org.gnome.settings-daemon.plugins.power idle-dim-time '0'
+gsettings set org.gnome.settings-daemon.plugins.power idle-dim 'false'
+gsettings set org.gnome.desktop.screensaver lock-delay '0'
+gsettings set org.gnome.desktop.screensaver lock-enabled 'false'
+gsettings set org.gnome.desktop.notifications show-banners 'false'
+
 sudo apt install unclutter chromium-browser xdotool
+
+mkdir -p ~/.config/autostart/
+cp RunKiosk.desktop ~/.config/autostart/
+cp RunKiosk.desktop ~/Desktop/
--- kiosk.sh
+++ kiosk.sh
@@ -1,29 +1,18 @@
 #!/bin/bash
- 
-# Run this script in display 0 - the monitor
+
 export DISPLAY=:0
- 
-# Hide the mouse from the display
+
 unclutter &
- 
-# If Chrome crashes (usually due to rebooting), clear the crash flag so we don't have the annoying warning bar
+
 sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/pi/.config/chromium/Default/Preferences
 sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/pi/.config/chromium/Default/Preferences
- 
-# Run Chromium and open tabs
+
 /usr/bin/chromium-browser --disable-translate --disable-infobars --touch-events=enabled \
     --disable-suggestions-service --disable-save-password-bubble --start-fullscreen \
-    --start-maximised --window-position=0,0 http://watson.ted.com &
+    --start-maximised --kiosk --window-position=0,0 $1 &
 
-# onboard &
- 
-# Start the kiosk loop. This keystroke changes the Chromium tab
-# To have just anti-idle, use this line instead:
-# xdotool keydown ctrl; xdotool keyup ctrl;
-# Otherwise, the ctrl+Tab is designed to switch tabs in Chrome
-# #
 while (true)
  do
-  xdotool keydown ctrl+Tab; xdotool keyup ctrl+Tab;
+  xdotool keydown ctrl; xdotool keyup ctrl;
   sleep 15
 done
